11cd1dc8d39092278b28a5d538b870aecdc661ce
Deepanshu Sinha <107sinha@gmail.com>
2020-09-19 12:20:00 +0530

    binary search in java solved

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,18 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int pivot, left = 0, right = nums.length - 1;
+        while(left <= right) {
+            pivot = left + (right - left) / 2;
+            if(target == nums[pivot])
+                return pivot;
+            if(target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return -1;
+    }
+}
