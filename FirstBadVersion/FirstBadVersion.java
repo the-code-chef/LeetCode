4038ba524cfc0959f3355d8e1f4e3a5a6e767489
Deepanshu Sinha <107sinha@gmail.com>
2020-09-25 12:20:00 +0530

    first bad version solved in java

---
+++ FirstBadVersion/FirstBadVersion.java
@@ -0,0 +1,18 @@
+package FirstBadVersion;
+
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int low = 1, high = n, mid;
+        
+        while(low < high) {
+            mid = low + (high - low) / 2;
+            if(isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+}
