d6ff79296582de70146df98b8780d685cbfad726
Deepanshu Sinha <107sinha@gmail.com>
2020-10-09 12:20:00 +0530

    search and insert position in java solved

---
+++ SearchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+package SearchInsertPosition;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while(left <= right) {
+            int pivot = left + (right - left) / 2;
+            if(target == nums[pivot]) {
+                return pivot;
+            } else if(target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return left;
+    }
+}
