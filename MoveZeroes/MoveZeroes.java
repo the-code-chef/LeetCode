5168cb5b82870d39b535ee88bc972f914fe82a69
Deepanshu Sinha <107sinha@gmail.com>
2020-11-03 12:20:00 +0530

    move zeroes solved in java

--- MoveZeroes/MoveZeroes.java
+++ MoveZeroes/MoveZeroes.java
@@ -1,5 +1,19 @@
 package MoveZeroes;
 
 public class MoveZeroes {
-    
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        
+        int n = nums.length;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        
+        while(j < n) {
+            nums[j++] = 0;
+        }
+    }
 }
