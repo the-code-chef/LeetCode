6824ffde2370a3f6048e0187509ec147bb6cc834
Deepanshu Sinha <107sinha@gmail.com>
2020-11-06 12:20:00 +0530

    reverse string solved in java

---
+++ ReverseString/ReverseString.java
@@ -0,0 +1,14 @@
+package ReverseString;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int j = s.length - 1;
+        int i = 0;
+        
+        while(i < j) {
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+    }
+}
