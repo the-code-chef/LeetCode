82d20331795956c7eb08950d68c190fa72c7bc5e
Deepanshu Sinha <107sinha@gmail.com>
2022-01-04 22:40:41 +0530

    Two sum II optimized

--- TwoSumII/TwoSumII.java
+++ TwoSumII/TwoSumII.java
@@ -3,18 +3,18 @@ package TwoSumII;
 public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
-        
+
         Map<Integer, Integer> map = new HashMap();
-        
-        for(int i = 0; i < numbers.length; i++) {
+
+        for (int i = 0; i < numbers.length; i++) {
             int temp = target - numbers[i];
-            if(map.containsKey(target - numbers[i])) {
+            if (map.containsKey(temp)) {
                 res[0] = map.get(temp);
-                res[1] = i+1;
+                res[1] = i + 1;
             }
-            map.put(numbers[i], i+1);
+            map.put(numbers[i], i + 1);
         }
-        
+
         return res;
     }
 }
